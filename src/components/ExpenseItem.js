3988f9cf114f45b3d35d07cf2df25cc51ebca573
ashwin695 <guptaashwin695@gmail.com>
2023-08-27 11:57:27 +0530

    second commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,31 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
+    const expenseDate = new Date(2023, 2, 6)
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.6
+    const LocationOfExpenditure = 'India'
+
     return(
-        <div>
-            <div>Date</div>
-            <div>
+        <div className='expense-item'>
+            <div>March 28th 2023</div>
+            <div className='expense-item_description'>
                 <h2>Food</h2>
-                <div>Rs. 10</div>
+                <div className='expense-item_price'>Rs. 10</div>
             </div>
-            <div>
+            <div className='expense-item_description'>
                 <h2>Petrol</h2>
-                <div>Rs. 100</div>
+                <div className='expense-item_price'>Rs. 100</div>
             </div>
-            <div>
+            <div className='expense-item_description'>
                 <h2>Movies</h2>
-                <div>Rs. 200</div>
+                <div className='expense-item_price'>Rs. 200</div>
+            </div>
+            <div className='expense-item_description'>
+                <h2>{expenseTitle}</h2>
+                <h2>{expenseDate}</h2>
+                <div className='expense-item_price'>Rs. {expenseAmount}</div>
+                <div className='expense-item_price'>{LocationOfExpenditure}</div>
             </div>
         </div>
     )
